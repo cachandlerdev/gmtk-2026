990fdf17be55af5d4f5c9ba90b857db48a0740b7
cachandlerdev <70250943+cachandlerdev@users.noreply.github.com>
2026-07-23 21:39:47 -0700

    feat: add game mode states for dynamically changing music (music is still very much wip because I'll add filler clips and might swap out the stealth track for something else)

--- scripts/game_mode.gd
+++ scripts/game_mode.gd
@@ -1,6 +1,23 @@
 extends Node
 
 
+@export_group("Music")
+# TODO: Change for multiple music tracks. Not the prettiest system but it'll work
+# TODO: Add a filler clip
+@export var exploration_music_index: int = 0
+@export var battle_music_index: int = 1
+@export var near_death_music_index: int = 2
+@export var escape_music_index: int = 3
+@export var victory_music_index: int = 4
+@export var defeat_music_index: int = 5
+@export var main_menu_music_index: int = 6
+@export var alarm_raised_music_index: int = 7
+
+
+enum GameModeState {Exploration, Battle, NearDeath, AlarmRaised, Escape, Victory, Defeat, MainMenu}
+var _game_mode_state := GameModeState.Exploration
+
+
 var _num_of_alert_guards: int = 0
 
 
@@ -11,8 +28,38 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(_num_of_alert_guards)
 	pass
+
+
+## Updates the game state and handles things like music changes 
+func set_state(new_state: GameModeState) -> void:
+	# TODO: Make this more sophisticated for multiple music tracks. 
+	# Not the prettiest system but it'll work for now
+	_game_mode_state = new_state
+	match new_state:
+		GameModeState.Exploration:
+			MusicPlayer.get_stream_playback().switch_to_clip(exploration_music_index)
+		GameModeState.Battle:
+			print("TODO: Battle stage")
+			MusicPlayer.get_stream_playback().switch_to_clip(battle_music_index)
+		GameModeState.NearDeath:
+			print("TODO: Near Death stage")
+			MusicPlayer.get_stream_playback().switch_to_clip(near_death_music_index)
+		GameModeState.AlarmRaised:
+			print("TODO: Alarm Raised stage")
+			MusicPlayer.get_stream_playback().switch_to_clip(alarm_raised_music_index)
+		GameModeState.Escape:
+			print("TODO: Escape stage")
+			MusicPlayer.get_stream_playback().switch_to_clip(escape_music_index)
+		GameModeState.Victory:
+			print("TODO: Victory stage")
+			MusicPlayer.get_stream_playback().switch_to_clip(victory_music_index)
+		GameModeState.Defeat:
+			print("TODO: Defeat stage")
+			MusicPlayer.get_stream_playback().switch_to_clip(defeat_music_index)
+		GameModeState.MainMenu:
+			print("TODO: Main Menu stage")
+			MusicPlayer.get_stream_playback().switch_to_clip(main_menu_music_index)
 
 
 ## Returns whether there are any guards that currently know where the player is.
@@ -24,9 +71,13 @@ func is_hidden() -> bool:
 ## list of alert guards.
 func add_watching_guard() -> void:
 	_num_of_alert_guards += 1
+	if _num_of_alert_guards == 1 and _game_mode_state == GameModeState.Exploration:
+		set_state(GameModeState.Battle)
 
 
 ## Used when a guard gives up and loses the player. Removes the specified guard
 ## from the list of alert guards.
 func remove_watching_guard() -> void:
 	_num_of_alert_guards -= 1
+	if _num_of_alert_guards == 0 and _game_mode_state == GameModeState.Battle:
+		set_state(GameModeState.Exploration)
